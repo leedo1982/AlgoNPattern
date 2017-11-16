c7f58a60915c34d661209d79d644a251d7ff7bca
domo18@gmail.com <domo18@gmail.com>
2017-11-16 23:49:53 +0900

    20171116

---
+++ src/BaekJoon0.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BaekJoon0 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        b = sc.nextInt();
+        System.out.println(b);
+    }
+}
--- src/BaekJoon10719.java
+++ src/BaekJoon10719.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class BaekJoon10718 {
+public class BaekJoon10719 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNextLine()) {
             String text = sc.nextLine();
-            if (!text.isEmpty() && text.trim().length() == text.length() && text.length() <= 100 ) {
+            if (text.length() <= 100) {
                 System.out.println(text);
             }
         }
