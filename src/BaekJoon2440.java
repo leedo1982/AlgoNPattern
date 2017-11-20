91a2845993d157926273c2462c8e8ffb3e507eff
domo18@gmail.com <domo18@gmail.com>
2017-11-20 18:10:12 +0900

    20171120

---
+++ src/BaekJoon2440.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BaekJoon2440 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        if (1 <= N && N <= 100) {
+            String star = "";
+            for (int i = 1; i <= N; i++) {
+                star += "*";
+            }
+            for (int y = 0; y < N; y++) {
+                System.out.println(star.substring(0, N - y));
+            }
+        }
+    }
+}
