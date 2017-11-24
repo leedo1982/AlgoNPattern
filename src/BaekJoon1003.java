b2c92f38bd53d3a099883833eed22ada538380c3
leedo1982 <domo18@gmail.com>
2017-11-24 09:22:24 +0900

    TIL20171124

--- src/BaekJoon1003.java
+++ src/BaekJoon1003.java
@@ -1,8 +1,18 @@
-import java.util.Scanner;
-
 public class BaekJoon1003 {
     public static void main(String[] args) {
+        fibonacci(1);
+    }
 
 
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            System.out.println("0");
+            return 0;
+        } else if (n == 1) {
+            System.out.println("1");
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
     }
 }
