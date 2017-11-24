d2a45453cc12e32ef782be2661e939a83c3cf659
leedo1982 <domo18@gmail.com>
2017-11-24 15:12:10 +0900

    ddd

--- src/BaekJoon1003.java
+++ src/BaekJoon1003.java
@@ -1,17 +1,14 @@
+import java.util.Scanner;
+
 public class BaekJoon1003 {
 
     static int zCnt = 0;
     static int oCnt = 0;
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        fibonacci(1);
-    }
-=======
         Scanner sc = new Scanner(System.in);
         int T;
         T = sc.nextInt();
->>>>>>> 8d0d107cdacb6ff11df085e1a1115badb5a10bbb
 
         for(int i = 0 ; i < T ; i++){
             int y ;
@@ -25,17 +22,10 @@ public class BaekJoon1003 {
 
     public static int fibonacci(int n) {
         if (n == 0) {
-<<<<<<< HEAD
-            System.out.println("0");
-            return 0;
-        } else if (n == 1) {
-            System.out.println("1");
-=======
             zCnt++;
             return 0;
         } else if (n == 1) {
             oCnt++;
->>>>>>> 8d0d107cdacb6ff11df085e1a1115badb5a10bbb
             return 1;
         } else {
             return fibonacci(n - 1) + fibonacci(n - 2);
