104843dade47436c1d64d054967b512913bad6b2
leedowon <domo18@gmail.com>
2018-06-22 16:50:46 +0900

    codility lesson6 triangle

---
+++ src/codility/Triangle.java
@@ -0,0 +1,18 @@
+package codility;
+
+import java.util.Arrays;
+
+public class Triangle {
+
+    public int solution(int[] A) {
+        Arrays.sort(A);
+
+        for (int i = 0, len = A.length; i < len - 2; i++) {
+            if (A[i] + A[i + 1] > A[i + 2]) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+}
